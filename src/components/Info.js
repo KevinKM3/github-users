f8006682c7a26484282861d5ca7ae5f8d8742b78
Kevin Matthews <kevmatthews3@gmail.com>
2020-06-03 14:46:35 -0400

    all-repos

--- src/components/Info.js
+++ src/components/Info.js
@@ -13,6 +13,30 @@ const Info = ({ data, repositories }) => {
       <tbody>
         <tr>
           <td>{data.name}</td>
+          <td>
+            {!data.avatar_url ? (
+              " "
+            ) : (
+              <img
+                className="ui small circular image"
+                src={data.avatar_url}
+                alt={data.avatar_url}
+              />
+            )}
+          </td>
+          <td>
+            {repositories.map((repo) => (
+              <div className="ui relaxed divided list" key={repo.name}>
+                <div className="item"></div>
+                <i className="large github middle aligned icon"></i>
+                <div className="content">
+                  <a href={repo.html_url} className="header" target="_blank">
+                    {repo.name}
+                  </a>
+                </div>
+              </div>
+            ))}
+          </td>
         </tr>
       </tbody>
     </table>
